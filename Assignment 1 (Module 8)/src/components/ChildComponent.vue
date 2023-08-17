0a31272fb9997117e293b18f4f55481f487493ff
mar-babu <ar_cse@yahoo.com>
2023-08-18 01:17:59 +0600

    Assignment 1 (Module 8)

---
+++ Assignment 1 (Module 8)/src/components/ChildComponent.vue
@@ -0,0 +1,29 @@
+<script setup>
+    defineProps(['message'])
+    defineEmits(['messageEmitted'])
+
+</script>
+
+<template>
+    <div class="border border-dashed rounded-lg p-4 mt-8 ml-8">
+        <h6 class="font-semibold mb-4">Child Component</h6>
+        <p class="text-sm text-gray-500 mb-4">{{ message }}</p>
+        <button
+            type="button"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none"
+            @click="
+            $emit(
+                'messageEmitted',
+                `Hi, from childComponent generating this custom message reply for: ${message} from parentComponent time: ${new Date().toLocaleString()}`
+            )
+            "
+        >
+            Generate Custom Message
+        </button>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ Assignment 1 (Module 8)/src/components/ParentComponent.vue
@@ -0,0 +1,23 @@
+<script setup>
+    import { ref } from 'vue'
+    import ChildComponent from './ChildComponent.vue'
+    
+    const message = ref('Hello, from parentComponent!')
+
+    const messageEmitted = (_message) => {
+        message.value = _message
+    }
+</script>
+
+<template>
+    <div class="border border-dashed rounded-lg p-11">
+        <h6 class="font-semibold mb-4">Parent Component</h6>
+        <p class="text-sm text-gray-500">{{ message }}</p>
+
+        <ChildComponent @message-emitted="messageEmitted" :message="message" />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
