2242b4f24032823b2f12bf70610cd5b334dc4235
mar-babu <ar_cse@yahoo.com>
2023-09-21 23:01:07 +0600

    View Details button added

--- Assignment 1 (Module 10)/src/components/Products.vue
+++ Assignment 1 (Module 10)/src/components/Products.vue
@@ -29,8 +29,13 @@
                         <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                             <img :src="`https://i.dummyjson.com/data/products/${product.id}/thumbnail.jpg`" alt="" class="h-full w-full object-cover object-center group-hover:opacity-75">
                         </div>
-                        <h3 class="mt-4 text-sm text-gray-700">{{ product.title }}</h3>
-                        <p class="mt-1 text-lg font-medium text-gray-900">${{ product.price }}</p>
+                        <div class="float-left">
+                            <h3 class="mt-4 text-sm text-gray-700">{{ product.title }}</h3>
+                            <p class="mt-1 text-lg font-medium text-gray-900">${{ product.price }}</p>
+                        </div>
+                        <div class="float-right">
+                            <router-link :to="{ name: 'product', params: { id: product.id }}" class="mt-10 text-sm font-medium text-blue-700 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">View Details</router-link> 
+                        </div>
                     </router-link> 
                 </span>
 
